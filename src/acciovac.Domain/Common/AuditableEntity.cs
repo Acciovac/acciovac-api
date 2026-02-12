3a5b3d054e009d1bc652c32c145f67aefffa3afe
Nirmal Ranasinghe <nirmalranasinha@gmail.com>
2026-02-12 23:04:50 +0530

    auditablke entiti added

---
+++ src/acciovac.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace acciovac.Domain.Common
+{
+    public abstract class AuditableEntity<TId> : Entity<TId>
+    {
+        public DateTime CreatedAt { get; protected set; }
+        public string CreatedBy { get; protected set; }
+
+        public DateTime? LastModifiedAt { get; protected set; }
+        public string? LastModifiedBy { get; protected set; }
+
+        protected void SetCreated(string userId)
+        {
+            CreatedAt = DateTime.UtcNow;
+            CreatedBy = userId;
+        }
+
+        protected void SetModified(string userId)
+        {
+            LastModifiedAt = DateTime.UtcNow;
+            LastModifiedBy = userId;
+        }
+    }
+}
