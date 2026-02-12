b7846a772f405e804272718610f2bf17a42290a1
Nirmal Ranasinghe <nirmalranasinha@gmail.com>
2026-02-12 23:04:34 +0530

    rate entity added

---
+++ src/acciovac.Domain/Entities/Rate.cs
@@ -0,0 +1,42 @@
+﻿using acciovac.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace acciovac.Domain.Entities
+{
+    public class Rate : AuditableEntity<Guid>
+    {
+        public decimal BaseFare { get; private set; }
+        public decimal PerKm { get; private set; }
+        public decimal? NightMultiplier { get; private set; }
+        public decimal? PeakMultiplier { get; private set; }
+
+        private Rate() { }
+
+        public Rate(
+            decimal baseFare,
+            decimal perKm,
+            decimal nightMultiplier,
+            decimal peakMultiplier)
+        {
+            Id = Guid.NewGuid();
+            BaseFare = baseFare;
+            PerKm = perKm;
+            NightMultiplier = nightMultiplier;
+            PeakMultiplier = peakMultiplier;
+        }
+
+        public void UpdateRates(
+            decimal baseFare,
+            decimal perKm,
+            decimal nightMultiplier,
+            decimal peakMultiplier)
+        {
+            BaseFare = baseFare;
+            PerKm = perKm;
+            NightMultiplier = nightMultiplier;
+            PeakMultiplier = peakMultiplier;
+        }
+    }
+}
