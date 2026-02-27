79f8727b3c1f30942e533479b7ef1649bf82d148
Nirmal Ranasinghe <nirmalranasinha@gmail.com>
2026-02-27 13:50:26 +0530

    global exception handler added

---
+++ src/acciovac.API/Middleware/GlobalExceptionHandler.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace acciovac.API.Middleware
+{
+    public class GlobalExceptionHandler : IExceptionHandler 
+    {
+        public async ValueTask<bool> TryHandleAsync(
+            HttpContext httpContext,
+            Exception exception,
+            CancellationToken cancellationToken)
+        {
+            if (exception is ValidationException validationException)
+            {
+                httpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+
+                var errors = validationException.Errors
+                    .GroupBy(e => e.PropertyName, e => e.ErrorMessage)
+                    .ToDictionary(failureGroup => failureGroup.Key, failureGroup => failureGroup.ToArray());
+
+                var problemDetails = new ValidationProblemDetails(errors)
+                {
+                    Type = "https://tools.ietf.org/html/rfc7231#section-6.5.1",
+                    Title = "One or more validation errors occurred.",
+                    Status = StatusCodes.Status400BadRequest,
+                    Instance = httpContext.Request.Path
+                };
+
+                await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+                return true;
+            }
+
+            return false;
+        }
+    }  
+}
