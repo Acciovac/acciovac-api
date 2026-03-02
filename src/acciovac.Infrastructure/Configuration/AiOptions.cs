dfbdda4d05a3ae63bab4090e38003f40cea96543
Nirmal Ranasinghe <nirmalranasinha@gmail.com>
2026-03-02 21:20:35 +0530

    Ai option configurations added

---
+++ src/acciovac.Infrastructure/Configuration/AiOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace acciovac.Infrastructure.Configuration
+{
+    public class AiOptions
+    {
+        public const string SectionName = "Gemini";
+        public string ApiKey { get; set; } = string.Empty;
+        public string ModelId { get; set; } = "gemini-1.5-flash";
+    }
+}
