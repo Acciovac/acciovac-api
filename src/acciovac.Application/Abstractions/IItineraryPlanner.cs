cd954783c874c1b971d0d4a4a23f33f03f0d1b2a
Nirmal Ranasinghe <nirmalranasinha@gmail.com>
2026-03-02 21:20:55 +0530

    itenery planner interface added

---
+++ src/acciovac.Application/Abstractions/IItineraryPlanner.cs
@@ -0,0 +1,12 @@
+﻿using acciovac.Domain.DTOs;
+
+namespace acciovac.Application.Abstractions
+{
+    public interface IItineraryPlanner
+    {
+        Task<List<AiDayDto>> GeneratePlanAsync(
+            string systemRules,
+            string userPrompt,
+            CancellationToken cancellationToken);
+    }
+}
