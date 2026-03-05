17cde0d162fd024e3c8d6fc58625d9a73c819e30
Nirmal Ranasinghe <nirmalranasinha@gmail.com>
2026-03-05 23:12:19 +0530

    Db context interface added

---
+++ src/acciovac.Application/Abstractions/IAppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using acciovac.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace acciovac.Application.Abstractions
+{
+    public interface IAppDbContext
+    {
+        DbSet<User> Users { get; }
+        DbSet<AiRule> AiRules { get; }
+        DbSet<Rate> Rates { get; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
--- src/acciovac.Infrastructure/Persistence/AppDbContext.cs
+++ src/acciovac.Infrastructure/Persistence/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using acciovac.Domain.Entities;
+﻿using acciovac.Application.Abstractions;
+using acciovac.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -6,8 +7,12 @@ using System.Text;
 
 namespace acciovac.Infrastructure.Persistence
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : DbContext , IAppDbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+               : base(options) { }
+
+
         public DbSet<User> Users => Set<User>();
         public DbSet<Role> Roles => Set<Role>();
         public DbSet<UserRole> UserRoles => Set<UserRole>();
@@ -16,8 +21,6 @@ namespace acciovac.Infrastructure.Persistence
         public DbSet<LocalExpirences> LocalExpirences => Set<LocalExpirences>();
         public DbSet<Message> Messages => Set<Message>();
 
-        public AppDbContext(DbContextOptions<AppDbContext> options)
-        : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
