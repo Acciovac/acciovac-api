825c5b51f3a8e2ff036c3582364859e91914a9e6
Nirmal Ranasinghe <nirmalranasinha@gmail.com>
2026-03-05 23:12:19 +0530

    Db context interface added

--- src/acciovac.Infrastructure/Persistence/AppDbContext.cs
+++ src/acciovac.Infrastructure/Persistence/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using acciovac.Domain.Entities;
+﻿using acciovac.Application.Abstractions;
+using acciovac.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -6,14 +7,16 @@ using System.Text;
 
 namespace acciovac.Infrastructure.Persistence
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : DbContext , IAppDbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+               : base(options) { }
+
+
         public DbSet<User> Users => Set<User>();
         public DbSet<AiRule> AiRules => Set<AiRule>();
         public DbSet<Rate> Rates => Set<Rate>();
 
-        public AppDbContext(DbContextOptions<AppDbContext> options)
-        : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
