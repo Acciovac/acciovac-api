9960f8beb632b3aff95fb4bd127b9ef2d177647a
Janidu-IT <JaniduJ-IT@LOLCGROUP.onmicrosoft.com>
2026-03-07 17:22:34 +0530

    update rate

---
+++ src/acciovac.Application/Behaviors/Rates/Commands/UpdateRate/UpdateRateCommand.cs
@@ -0,0 +1,16 @@
+using acciovac.Application.Common;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace acciovac.Application.Behaviors.Rates.Commands.UpdateRate
+{
+    public sealed record UpdateRateCommand(
+        Guid RateId,
+        decimal BaseFare,
+        decimal PerKm
+        // decimal? NightMultiplier,
+        // decimal? PeakMultiplier
+    ) : IRequest<Result<Guid>>;
+}
