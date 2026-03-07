4bbd987c2e072e0f0091ca6d712be58dc77f9836
Janidu-IT <JaniduJ-IT@LOLCGROUP.onmicrosoft.com>
2026-03-07 17:37:14 +0530

    add post method for update the price

--- src/acciovac.API/Controllers/RatesController.cs
+++ src/acciovac.API/Controllers/RatesController.cs
@@ -41,6 +41,29 @@ namespace acciovac.API.Controllers
             });
         }
 
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> CreateRate([FromBody] CreateRateRequest request)
+        {
+            var rate = new Rate(
+                request.BaseFare,
+                request.PerKm,
+                // request.NightMultiplier ?? 1.0m,
+                // request.PeakMultiplier ?? 1.0m,
+                "system"
+            );
+
+            await _rateRepository.AddAsync(rate);
+
+            return CreatedAtAction(
+                nameof(GetActiveRate),
+                new { id = rate.Id },
+                new { id = rate.Id, message = "Rate created successfully" });
+        }
+
+        
+
         
     }
 
