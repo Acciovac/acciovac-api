003995f95dc70f08599bc694f41f2683880442e7
Janidu-IT <JaniduJ-IT@LOLCGROUP.onmicrosoft.com>
2026-03-10 14:13:25 +0530

    create LocalExpirences domain

---
+++ src/acciovac.Domain/Entities/LocalExpirences.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace acciovac.Domain.Entities;
+
+public class LocalExpirences : AuditableEntity<Guid>
+    {
+        public decimal BaseFare { get; private set; }
+        public decimal PerKm { get; private set; }
+        public decimal? NightMultiplier { get; private set; }
+        public decimal? PeakMultiplier { get; private set; }
+
+        private Rate() { }
+
+        public Rate(
+            decimal baseFare,
+            decimal perKm,
+            decimal nightMultiplier,
+            decimal peakMultiplier,
+            string createdBy = "system")
+        {
+            Id = Guid.NewGuid();
+            BaseFare = baseFare;
+            PerKm = perKm;
+            NightMultiplier = nightMultiplier;
+            PeakMultiplier = peakMultiplier;
+            SetCreated(createdBy);
+        }
+
+        public void UpdateRates(
+            decimal baseFare,
+            decimal perKm,
+            decimal nightMultiplier,
+            decimal peakMultiplier,
+            string modifiedBy = "system")
+        {
+            BaseFare = baseFare;
+            PerKm = perKm;
+            NightMultiplier = nightMultiplier;
+            PeakMultiplier = peakMultiplier;
+            SetModified(modifiedBy);
+        }
+}
