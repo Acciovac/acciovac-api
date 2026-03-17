16ff4a803140af00557d834fb2043014100e94ce
Nirmal Ranasinghe <nirmalranasinha@gmail.com>
2026-03-17 23:51:32 +0530

    Add services to the dependancy injection

--- src/acciovac.Infrastructure/DependencyInjection.cs
+++ src/acciovac.Infrastructure/DependencyInjection.cs
@@ -4,9 +4,6 @@ using acciovac.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace acciovac.Infrastructure
 {
