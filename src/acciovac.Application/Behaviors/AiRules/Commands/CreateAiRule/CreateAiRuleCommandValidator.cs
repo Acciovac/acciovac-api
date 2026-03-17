9b2794de02bced105d1ba89bcd78a1e488d68b1c
Nirmal Ranasinghe <nirmalranasinha@gmail.com>
2026-03-17 23:52:03 +0530

    Create Ai Rull Command added

---
+++ src/acciovac.Application/Behaviors/AiRules/Commands/CreateAiRule/CreateAiRuleCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace acciovac.Application.Behaviors.AiRules.Commands.CreateAiRule
+{
+    public class CreateAiRuleCommandValidator : AbstractValidator<CreateAiRuleCommand>
+    {
+        public CreateAiRuleCommandValidator() 
+        {
+            RuleFor(x => x.Code)
+            .NotEmpty()
+            .MaximumLength(50);
+
+            RuleFor(x => x.RuleText)
+                .NotEmpty();
+
+            RuleFor(x => x.Priority)
+                .GreaterThanOrEqualTo(0);
+        }
+    }
+}
