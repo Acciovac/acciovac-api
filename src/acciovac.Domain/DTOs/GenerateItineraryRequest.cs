45bb0bb7b99afa02615ea3be44a4a5fb652f7e98
Nirmal Ranasinghe <nirmalranasinha@gmail.com>
2026-03-20 21:58:32 +0530

    change api endpoint and refactor code

---
+++ src/acciovac.Domain/DTOs/GenerateItineraryRequest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace acciovac.Domain.DTOs
+{
+    public class GenerateItineraryRequest
+    {
+        public DateRangeRequest? DateRange { get; set; }
+        public string? Budget { get; set; }
+        public string? TravelType { get; set; }
+        public List<string>? Interests { get; set; }
+        public LocationRequest? StartLocation { get; set; }
+        public LocationRequest? EndLocation { get; set; }
+        public Dictionary<string, DailyFreeTimeRequest>? DailyFreeTimes { get; set; }
+    }
+
+    public class DateRangeRequest
+    {
+        public string? Start { get; set; }
+        public string? End { get; set; }
+    }
+
+    public class LocationRequest
+    {
+        public string? Name { get; set; }
+        public string? Type { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+
+    public class DailyFreeTimeRequest
+    {
+        public string? Start { get; set; }
+        public string? End { get; set; }
+    }
+}
