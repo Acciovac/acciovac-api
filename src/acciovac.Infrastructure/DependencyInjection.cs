61e55d17946b92ab77a25b144fc9197ef158b5e2
Janidu-IT <JaniduJ-IT@LOLCGROUP.onmicrosoft.com>
2026-03-20 22:33:32 +0530

    dependancy injection

--- src/acciovac.Infrastructure/DependencyInjection.cs
+++ src/acciovac.Infrastructure/DependencyInjection.cs
@@ -23,6 +23,7 @@ namespace acciovac.Infrastructure
             services.AddScoped<IRateRepository, RateRepository>();
             services.AddScoped<ILocalExpirences, LocalExpirencesRepository>();
             services.AddScoped<IAiRuleRepository, AiRuleRepository>();
+            services.AddScoped<IMessageRepository, MessageRepository>();
 
             services.AddHttpClient<IGoogleMapsService, GoogleMapsService>();
             services.AddHttpClient<IWeatherService, WeatherService>();
