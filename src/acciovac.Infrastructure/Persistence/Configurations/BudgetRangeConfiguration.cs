038172c4e249d066f739379d02e87f2412eaa0f6
Nirmal Ranasinghe <nirmalranasinha@gmail.com>
2026-03-22 18:13:11 +0530

    budget range configurations added

---
+++ src/acciovac.Infrastructure/Persistence/Configurations/BudgetRangeConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using acciovac.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace acciovac.Infrastructure.Persistence.Configurations
+{
+    public class BudgetRangeConfiguration : IEntityTypeConfiguration<BudgetRange>
+    {
+        public void Configure(EntityTypeBuilder<BudgetRange> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Type)
+                .IsRequired();
+
+            builder.Property(x => x.FromBudget)
+                .HasColumnType("decimal(18,2)")
+                .IsRequired();
+
+            builder.Property(x => x.ToBudget)
+                .HasColumnType("decimal(18,2)")
+                .IsRequired();
+        }
+    }
+}
