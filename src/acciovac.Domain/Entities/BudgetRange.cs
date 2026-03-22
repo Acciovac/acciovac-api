810e86f9660890879c2acb67d63db43cc39d8b47
Nirmal Ranasinghe <nirmalranasinha@gmail.com>
2026-03-22 18:11:44 +0530

    Budget range entity added

---
+++ src/acciovac.Domain/Entities/BudgetRange.cs
@@ -0,0 +1,17 @@
+﻿using acciovac.Domain.Common;
+using acciovac.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace acciovac.Domain.Entities
+{
+    public class BudgetRange : AuditableEntity<int>
+    {
+        public int Id { get; set; }
+        public byte Type { get; set; }
+        public decimal FromBudget { get; set; }
+        public decimal ToBudget { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
